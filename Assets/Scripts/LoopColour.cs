1edf0e791a5811cb174bb87b0c0f02698a681eba
Max <madmanmax120@hotmail.com>
2020-07-11 13:59:02 +1000

    Made the loops brother (sprite + colour only)

---
+++ Assets/Scripts/LoopColour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopColour : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        float random = Random.Range(0f, 255f);
+        float saturation = 0.7f;
+        if (random >= 80 && random <= 180)
+        {
+            saturation -= 0.2f;
+        }
+        spriteRenderer.color = Color.HSVToRGB(random/255f, saturation, 1);
+    }
+}
