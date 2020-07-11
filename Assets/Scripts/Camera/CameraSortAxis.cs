4027037f43aa337ef696e70d7bcb88282b89b39d
Max <madmanmax120@hotmail.com>
2020-07-12 08:38:24 +1000

    Updates Sorting Axis - Sprites need some fixing

---
+++ Assets/Scripts/Camera/CameraSortAxis.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSortAxis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.transparencySortMode = TransparencySortMode.CustomAxis;
+        Camera.main.transparencySortAxis = Vector3.up;
+    }
+}
