28c6a32407e0220500544049e270fb8565d9000e
Ivan <karyakinivan@live.com>
2020-07-12 17:56:55 +1000

    Added logic for tutorial

---
+++ Assets/Scripts/UI/TutorialShow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialShow : MonoBehaviour
+{
+    private bool tutToggle = false;
+    public GameObject main, tutorial;
+
+    // Update is called once per frame
+
+    public void Toggle()
+    {
+        tutToggle = !tutToggle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tutToggle)
+        {
+            main.SetActive(false);
+            tutorial.SetActive(true);
+        }
+        else
+        {
+            main.SetActive(true);
+            tutorial.SetActive(false);
+        }    
+    }
+}
