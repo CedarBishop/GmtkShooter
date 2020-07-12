d441d1a91c6f4be9b5843e7eda752117f38cf2a2
CedarBishop <cedar.bishop@outlook.com>
2020-07-12 16:59:24 +1000

    Created Bacon AI

---
+++ Assets/Scripts/AI/AI_Trident.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Trident : MonoBehaviour
+{
+    public Transform aimOrigin;
+    public int damage;
+
+    private PlayerHealth player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerHealth>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (player.transform.position - aimOrigin.position).normalized;
+        aimOrigin.right = direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerHealth>())
+        {
+            collision.GetComponent<PlayerHealth>().TakeDamage(damage);
+        }
+    }
+}
