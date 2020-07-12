b2fef43dadcab9e32285b003b33295d4a2db64c3
Max <madmanmax120@hotmail.com>
2020-07-12 19:14:42 +1000

    Added Splat sprites and random flip to Splat Prefab

---
+++ Assets/Scripts/Juice/SplatRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplatRandomizer : MonoBehaviour
+{
+    public Sprite[] splats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = splats[Random.Range(0, splats.Length - 1)];
+        int temp = (int)Random.Range(0, 2);
+        GetComponent<SpriteRenderer>().flipY = (temp == 0) ? false : true;
+    }
+}
