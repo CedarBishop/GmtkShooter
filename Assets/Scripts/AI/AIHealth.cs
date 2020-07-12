07521fb2e5292f19f8764742855f1d1ea209277b
Ivan <karyakinivan@live.com>
2020-07-12 17:21:31 +1000

    Added Floating Score

--- Assets/Scripts/AI/AIHealth.cs
+++ Assets/Scripts/AI/AIHealth.cs
@@ -6,11 +6,14 @@ public class AIHealth : HealthSystem
 {
     private AI ai;
     private int score;
+    public GameObject floatingPoints;
 
     protected override void Death()
     {
         ai = GetComponent<AI>();
         score = ai.score;
+        floatingPoints.transform.GetChild(0).GetComponent<TextMesh>().text = "+" + score.ToString();
+        Instantiate(floatingPoints, transform.position, Quaternion.identity);
         GameManager.instance.waveSystem.AIDied(score);
         base.Death();
     }
